cf0000e698d43b163edf3aac72195f3f0f7f1f8a
Derek Daniels <dbdaniel@mtu.edu>
2020-04-18 16:21:14 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dbd.sh
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :20300...")
+	err := http.ListenAndServe(":20300", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
