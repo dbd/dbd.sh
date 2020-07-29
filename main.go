34c3798a085cfd4c9bcc9cc745b3c4058f1cb901
Derek Daniels <dbdaniel@mtu.edu>
2020-07-28 23:34:49 -0500

    Log entire request

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		log.Printf("%+v\n", r)
 		handler.ServeHTTP(w, r)
 	})
 }
