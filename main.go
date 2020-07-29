920cad471c44f84f1dab8851ec40fe0d455b00f0
Derek Daniels <dbdaniel@mtu.edu>
2020-07-28 22:15:59 -0500

    Add basic logging

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
+func logRequest(handler http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
 	log.Println("Listening on :20300...")
-	err := http.ListenAndServe("0.0.0.0:20300", nil)
+	err := http.ListenAndServe("0.0.0.0:20300", logRequest(http.DefaultServeMux))
 	if err != nil {
 		log.Fatal(err)
 	}
